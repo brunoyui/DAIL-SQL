dfbf681d4587a523bf45753c0e8433f98c4d93cf
byamate <byamate@santander.com.br>
2024-05-04 20:08:42 -0300

    adjustments

--- llm/gemini.py
+++ llm/gemini.py
@@ -25,7 +25,7 @@ def get_model(api_key, model):
         },
     ]
     generation_config = {}
-    if model == LLM.GEMINI_1_0_PRO | model == GEMINI_1_0_PRO_001:
+    if model == LLM.GEMINI_1_0_PRO or model == GEMINI_1_0_PRO_001:
         generation_config = {
             "temperature": 0,
             "top_p": 1,
